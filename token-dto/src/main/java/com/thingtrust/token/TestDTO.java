b9e98efcbb11121ca72b1e5a06d4dd9beebd7b2a
WangYu <yu.wang@thingtrust.com>
2018-05-16 15:49:00 +0800

    submit DTO model

---
+++ token-dto/src/main/java/com/thingtrust/token/TestDTO.java
@@ -0,0 +1,16 @@
+package com.thingtrust.token;
+
+import lombok.Data;
+
+/**
+ * 〈〉
+ *
+ * @author WangYu
+ * @create 2018/5/16
+ * @since 1.0.0
+ */
+@Data
+public class TestDTO {
+
+    private Long id;
+}
